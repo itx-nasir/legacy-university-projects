b3619d9afb0cae5cc4889c1215668511d8f7fc58
NasirXpython <Nasir.Iqbal.Dev@gmail.com>
2022-02-03 16:37:00 +0500

    Fixed memory leaks

---
+++ Java/TORCS_SIMULATOR_CLIENT/src/com/ai/sd/model/SensorModel.java
@@ -0,0 +1,52 @@
+package com.ai.sd.model;
+
+public interface SensorModel {
+
+    // basic information about your car and the track (you probably should take care of these somehow)
+
+    public double getSpeed ();
+
+    public double getAngleToTrackAxis ();
+
+    public double[] getTrackEdgeSensors ();
+    
+    public double[] getFocusSensors ();//ML
+
+    public double getTrackPosition();
+
+    public int getGear ();
+
+    // basic information about other cars (only useful for multi-car races)
+
+    public double[] getOpponentSensors ();
+
+    public int getRacePosition ();
+
+    // additional information (use if you need)
+
+    public double getLateralSpeed ();
+    
+
+    public double getCurrentLapTime ();
+
+    public double getDamage ();
+
+    public double getDistanceFromStartLine ();
+
+    public double getDistanceRaced ();
+
+    public double getFuelLevel ();
+
+    public double getLastLapTime ();
+
+    public double getRPM ();
+
+    public double[] getWheelSpinVelocity ();
+    
+    public double getZSpeed ();
+    
+    public double getZ ();
+    
+    public String getMessage();
+    
+}
