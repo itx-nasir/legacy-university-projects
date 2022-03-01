3953386d0c0808ef5117f6b43c9d50f6c14f06ec
NasirXpython <Nasir.Iqbal.Dev@gmail.com>
2022-03-01 10:02:00 +0500

    Updated project files and documentation

---
+++ Java/TORCS_SIMULATOR_CLIENT/src/com/ai/sd/model/DangerzoneCause.java
@@ -0,0 +1,19 @@
+package com.ai.sd.model;
+public enum DangerzoneCause {
+	
+	CarCrash,OutOfTrack,Jump;
+	
+	static DangerzoneCause fromInt(int value)
+	{
+		switch (value) {
+		case 0:
+			return CarCrash;
+		case 1:
+			return OutOfTrack;
+		case 2:
+			return Jump;
+		default:
+			return OutOfTrack;
+		}			
+	}
+};
